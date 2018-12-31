27051f7c028e6a4c4ebb581a084f23153201c79e
Hamid Hadi <hamidhadi@live.com>
2018-12-31 10:55:22 +0330

    Simplify layout_table_generator :)

--- layout_table_generator.js
+++ layout_table_generator.js
@@ -9,23 +9,15 @@ module.exports = (sections, itemHeight, sectionHeaderHeight, sectionFooterHeight
     })
 
     for(let i = 0; i < section.data.length; i++) {
-      let currentSectionLayoutTableLastItem = currentSectionLayoutTable[currentSectionLayoutTable.length - 1]
+      let CSLTLength = currentSectionLayoutTable.length
 
       currentSectionLayoutTable.push({
         length: itemHeight,
-        offset: currentSectionLayoutTableLastItem.offset + currentSectionLayoutTableLastItem.length
+        offset: currentSectionLayoutTable[CSLTLength - 1].offset + currentSectionLayoutTable[CSLTLength - 1].length
       })
-
-      // if(i !== section.data.length - 1) {
-      //   currentSectionLayoutTableLastItem = currentSectionLayoutTable[currentSectionLayoutTable.length - 1]
-      //   currentSectionLayoutTable.push({
-      //     length: 0,
-      //     offset: currentSectionLayoutTableLastItem.offset + currentSectionLayoutTableLastItem.length
-      //   })
-      // }
     }
 
-    const currentSectionLayoutTableLastItem = currentSectionLayoutTable[currentSectionLayoutTable.length - 1]
+    CSLTLength += 1
     currentSectionLayoutTable.push({
       length: sectionFooterHeight,
       offset: currentSectionLayoutTableLastItem.offset + currentSectionLayoutTableLastItem.length
